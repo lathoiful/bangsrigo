d552933bfc0f0bb55acf5e0121ba89fd66ad9326
AfnanYusuf01 <Hermanaaji3@gmail.com>
2026-08-04 03:48:03 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  integrations: [
+    icon(), // Add integration di sini
+  ],
+});
