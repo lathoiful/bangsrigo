97884a812258167ddf0601127bbc24b6468be27f
AfnanYusuf01 <Hermanaaji3@gmail.com>
2026-08-05 00:27:17 +0700

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,21 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   integrations: [
     icon(), // Add integration di sini
   ],
+
+  adapter: vercel(),
 });
